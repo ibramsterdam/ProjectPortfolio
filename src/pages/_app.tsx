abc209d139b8c4f5a59e01afcb8b7b4aba2d3380
Bram Janssen <bram.janssen2@hva.nl>
2021-12-06 21:01:27 +0100

    add favicon file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import Theme from '../styles/theme';
 import { AppProps } from 'next/app';
 
+const prefix = '/ProjectPortfolio';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <link rel="shortcut icon" href={prefix + '/favicon.ico'} />
       <Theme>
         <Component {...pageProps} />
       </Theme>
